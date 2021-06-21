d14ad98a144369c8935b62d65c7e4003eb9e6df0
Алексей Гулецкий <a9805@mail.ru>
2021-06-21 16:52:30 +0300

    123

---
+++ Pokemon/ViewModels/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Pokemon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pokemon.ViewModels
+{
+    public class OrderViewModel
+    {
+        public Order Order { get; set; }
+        public Order Name { get; set; }
+        public Order OrderDate { get; set; }
+    }
+}
